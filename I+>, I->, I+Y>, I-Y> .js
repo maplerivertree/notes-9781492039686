2eb64b147660598ef7a4507c1a1c90f279ea71ed
maplerivertree <44853896+maplerivertree@users.noreply.github.com>
2019-12-05 17:48:35 -0800

    Create I+>, I->, I+Y>, I-Y> .js

---
+++ I+>, I->, I+Y>, I-Y> .js
@@ -0,0 +1,26 @@
+/// ---- rotating leftward
+
+//    |+>
+qc.reset(1);
+qc.write(0)
+qc.had();
+
+//    |->
+qc.reset(1);
+qc.write(0)
+qc.had();
+qc.phase(180)
+
+//    |+Y>
+qc.reset(1);
+qc.write(0)
+qc.had();
+qc.phase(90)
+
+
+//    |-Y>
+
+qc.reset(1);
+qc.write(0)
+qc.had();
+qc.phase(270)
